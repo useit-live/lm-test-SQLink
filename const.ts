605d9b2a426dfad96e38d6ecc17458b6258a9c4d
Vitalii Adler <useit.live.ai@gmail.com>
2023-12-26 21:19:33 +0200

    only abc

--- const.ts
+++ const.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export const FormSchema = z.object({
-  input: z.string().min(4, 'minLength: 4, maxLength: 8, only latin letters'),
+  input: z.string()
+    .min(4, 'minLength: 4, maxLength: 8, only latin letters')
+    .refine((value) => /^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/.test(value), 'Name should contain only alphabets'),
   customColorName: z.string(),
   isVerified: z.boolean().refine((bool) => bool, {
     message: 'You must accept something',
